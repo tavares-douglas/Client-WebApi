22c4ced524900a70654bc40c3403655eece84301
Douglas Tavares Martins <douglas.tavares@inatel.br>
2023-02-23 23:07:55 -0300

    fix: fixes POST method tests

--- TechnicalCase-PBTech.Tests/Controllers/ClientsControllerTests.cs
+++ TechnicalCase-PBTech.Tests/Controllers/ClientsControllerTests.cs
@@ -1,18 +1,10 @@
 ﻿using FakeItEasy;
-using Google;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TechnicalCase_PBTech.Controllers;
 using TechnicalCase_PBTech.Data;
 using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 
-namespace TechnicalCase_PBTech.Tests.Controller
+namespace TechnicalCase_PBTech.Tests.Controllers
 {
     [TestClass]
     public class ClientControllerTest
@@ -37,7 +29,7 @@ namespace TechnicalCase_PBTech.Tests.Controller
             dataContext.Add(clients);
             ClientController clientController = new ClientController(dataContext);
 
-            var queryResult = clientController.GetClient("jalenhurts@eagles.com");
+            var queryResult = clientController.GetClient(clients[0].Email);
             Assert.Equals(queryResult, clients[0]);
         }
 
@@ -49,7 +41,8 @@ namespace TechnicalCase_PBTech.Tests.Controller
             dataContext.Add(clients);
             ClientController clientController = new ClientController(dataContext);
 
-            var queryResult = clientController.GetClient("brettmaher@cowboys.com");
+            String nonExistingClientToBeDeleted = "brettmaher@cowboys.com");
+            var queryResult = clientController.GetClient(nonExistingClientToBeDeleted);
             Assert.Equals(queryResult, "Client not found.");
         }
 
@@ -61,7 +54,7 @@ namespace TechnicalCase_PBTech.Tests.Controller
             dataContext.Add(clients);
             ClientController clientController = new ClientController(dataContext);
 
-            Client clientToBeAdded = new Client { Email = "larryfitzgerald@cardinals.com", Name = "Larry Fitzgerald" });
+            Client clientToBeAdded = new Client { Email = "larryfitzgerald@cardinals.com", Name = "Larry Fitzgerald" };
             var queryResult = clientController.AddClient(clientToBeAdded);
             clients.Add(clientToBeAdded);
             Assert.Equals(queryResult, clients);
@@ -75,11 +68,37 @@ namespace TechnicalCase_PBTech.Tests.Controller
             dataContext.Add(clients);
             ClientController clientController = new ClientController(dataContext);
 
-            Client clientToBeAdded = new Client { Email = "larryfitzgerald", Name = "Larry Fitzgerald" });
+            Client clientToBeAdded = new Client { Email = "larryfitzgerald", Name = "Larry Fitzgerald" };
             var queryResult = clientController.AddClient(clientToBeAdded);
-            clients.Add(clientToBeAdded);
             Assert.Equals(queryResult, "E-mail not allowed.");
         }
+
+        [TestMethod]
+        public void ClientController_AddClient_ReturnsEmailNotAllowedBecauseOfDotSymbol()
+        {
+            var clients = GetTestClients();
+            var dataContext = A.Fake<DataContext>();
+            dataContext.Add(clients);
+            ClientController clientController = new ClientController(dataContext);
+
+            Client clientToBeAdded = new Client { Email = "larryfitzgerald@outlook", Name = "Larry Fitzgerald" };
+            var queryResult = clientController.AddClient(clientToBeAdded);
+            Assert.Equals(queryResult, "E-mail not allowed.");
+        }
+
+        //[TestMethod]
+        //public void ClientController_UpdateClient_ReturnsOk()
+        //{
+        //    var clients = GetTestClients();
+        //    var dataContext = A.Fake<DataContext>();
+        //    dataContext.Add(clients);
+        //    ClientController clientController = new ClientController(dataContext);
+
+        //    String clientToBeUpdatedEmail = "larryfitzgerald@outlook.com";
+        //    Client clientToBeUpdated = new Client { Email = "larryfitzpatrick@outlook.com", Name = "Larry Fitzpatrick" };
+        //    var queryResult = clientController.UpdateClient(clientToBeUpdatedEmail, clientToBeUpdated);
+        //    Assert.Equals(queryResult, "E-mail not allowed.");
+        //}
 
         [TestMethod]
         public void ClientController_DeleteClient_ReturnsOk()
@@ -89,8 +108,7 @@ namespace TechnicalCase_PBTech.Tests.Controller
             dataContext.Add(clients);
             ClientController clientController = new ClientController(dataContext);
 
-            var client = new Client();
-            var queryResult = clientController.DeleteClient("jjwatt@cardinals");
+            var queryResult = clientController.DeleteClient(clients[2].Email);
             clients.RemoveAt(2);
             Assert.Equals(queryResult, clients);
         }
@@ -103,7 +121,8 @@ namespace TechnicalCase_PBTech.Tests.Controller
             dataContext.Add(clients);
             ClientController clientController = new ClientController(dataContext);
 
-            var queryResult = clientController.DeleteClient("brettmaher@cowboys.com");
+            String nonExistingClientToBeDeleted = "brettmaher@cowboys.com";
+            var queryResult = clientController.DeleteClient(nonExistingClientToBeDeleted);
             Assert.Equals(queryResult, "Client not found.");
         }
 
@@ -112,7 +131,7 @@ namespace TechnicalCase_PBTech.Tests.Controller
         private List<Client> GetTestClients()
         {
             var testClients = new List<Client>();
-            testClients.Add(new Client { Email = "jalenhurts@eagles.com", Name = "Jalen Hurts"});
+            testClients.Add(new Client { Email = "jalenhurts@eagles.com", Name = "Jalen Hurts" });
             testClients.Add(new Client { Email = "tuatagovailoa@dolphins.com", Name = "Tua Tagovailoa" });
             testClients.Add(new Client { Email = "jjwatt@cardinals", Name = "JJ Watt" });
 
